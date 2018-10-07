c0a2b09e2ddbd7d76bda0df4fb94014599a983de
richard <chaaaardkun@gmail.com>
2018-10-07 21:16:28 +0800

    Added new objects and obstacles in the map. Added lights and adjusted its intensity and range. Added jumpscare scripts and sounds for later

--- Assets/Jumpscare.cs
+++ Assets/Jumpscare.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class Jumpscare : MonoBehaviour {
     public GameObject e;
+    public bool entered;
 	// Use this for initialization
-	void Start () {
-		
-	}
+    public AudioClip alarm;
+    private AudioSource source;
+ 
+     // Use this for initialization
+     void Start () {
+         entered = false;
+         source = GetComponent<AudioSource>();
+     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
     private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            Instantiate(e, transform.position, transform.rotation);
-            
+    {   
+        //checks whether the player has already entered the trigger
+        if(entered == false){
+            if(other.tag == "Player")
+            {
+                //sets to true so it plays only once
+                entered = true;
+                Instantiate(e, transform.position, transform.rotation);
+                source.PlayOneShot (alarm);  
+                Destroy(gameObject, alarm.length);
+                
+            }
         }
     }
 }
